46fda6a8a489ae6c6d8c7760bcc42b99ba04ef67
MedhaShrivastava <medhakumari299@gmail.com>
2021-07-15 21:09:21 +0530

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Services from './components/Services';
+
+
+const Home = () => {
+
+  return (
+    <>
+    
+      <Services  invertMobile topDivider imageFill className="illustration-section-02" />
+    
+    </>
+  );
+}
+
+export default Home;
